1665b28101eca7bd0860558c0eee0e55f708a03c
yinwy <yinwy@t3go.cn>
2020-05-06 10:22:34 +0800

    chore: 文件修改

---
+++ src/url.md
@@ -0,0 +1,2 @@
+跨域:
+https://juejin.im/post/5e948bbbf265da47f2561705
--- src/util/decorator.js
+++ src/util/decorator.js
@@ -27,26 +27,6 @@ A = decorator(A) || A;
 
 
 
-// mixins.js
-export function mixins(...list) {
-  return function (target) {
-    Object.assign(target.prototype, ...list)
-  }
-}
-
-// main.js
-import { mixins } from './mixins'
-
-const Foo = {
-  foo() { console.log('foo') }
-};
-
-@mixins(Foo)
-class MyClass {}
-
-let obj = new MyClass();
-obj.foo() // 'foo'
-
 
 
 
